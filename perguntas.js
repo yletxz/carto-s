9f4735558cb071a0260808079ed2ca99beb3bf54
yletxz <143011416+yletxz@users.noreply.github.com>
2024-10-28 08:39:57 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Bι᥆ᥣ᥆gιᥲ',
+    '𝑸𝒖𝒂𝒏𝒕𝒐𝒔 𝒐𝒔𝒔𝒐𝒔 𝒕𝒆𝒎 𝒐 𝒄𝒐𝒓𝒑𝒐 𝒅𝒆 𝒖𝒎 𝒂𝒅𝒖𝒍𝒕𝒐 𝒆 𝒅𝒆 𝒖𝒎 𝒓𝒆𝒄𝒆𝒎 𝒏𝒂𝒔𝒄𝒊𝒅𝒐?',
+    'O esqueleto de um recem nascido tem cerca de 270 ossos, enquanto que um adulto possui cerca de 206.',
+)
+
+criaCartao(
+    'Bι᥆ᥣ᥆gιᥲ',
+    '𝑶 𝒒𝒖𝒆 𝒆 𝒄𝒆𝒍𝒖𝒍𝒂?',
+    'é a unidade básica da vida.',
+)
+
+criaCartao(
+    'Bι᥆ᥣ᥆gιᥲ',
+    '𝒒𝒖𝒂𝒍 𝒂 𝒇𝒖𝒏ç𝒂̃𝒐 𝒅𝒐 𝑫𝑵𝑨?',
+    ' armazenar de informaçoẽs geneticas, codificação de proteínas, replicação, regulação da atividade celular, herença genética,diversidade genética e autocorreção',
+)
+
+criaCartao(
+    'Bι᥆ᥣ᥆gιᥲ',
+    ' 𝒐 𝒒𝒖𝒆 𝒔𝒂̃𝒐 𝒐𝒓𝒈𝒂𝒏𝒆𝒍𝒂𝒔 𝒄𝒆𝒍𝒖𝒍𝒂𝒓𝒆𝒔?',
+    'Organelas celulares são estruturas envolvidas por membranas, localizadas no citoplasma de células eucarióticas e que desempenham importantes funções relacionadas com a sobrevivência da célula.',
+)
+
+criaCartao(
+    'Bι᥆ᥣ᥆gιᥲ',
+    pergunta,
+    resposta
+)
+
+criaCartao(
+    'Bι᥆ᥣ᥆gιᥲ',
+    pergunta,
+    resposta
+)
+
+criaCartao(
+    'Bι᥆ᥣ᥆gιᥲ',
+    pergunta,
+    resposta
+)
+
+criaCartao(
+    'Bι᥆ᥣ᥆gιᥲ',
+    pergunta,
+    resposta
+)
+
+criaCartao(
+    'Bι᥆ᥣ᥆gιᥲ',
+    pergunta,
+    resposta
+)
